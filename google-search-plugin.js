899caebfdd97bf7b84ceb9d282ae60d3d966a2f8
fobo4ka <fobo4ka@gmail.com>
2014-12-07 22:20:24 +0300

    async load google api

--- google-search-plugin.js
+++ google-search-plugin.js
@@ -1,10 +1,24 @@
 (function ($) {
-	google.load('search', '1');
+	var deferred = $.Deferred();
+	function onLoaded() {
+		deferred.resolve();
+	}
+
+	$.getScript('https://www.google.com/jsapi', function () {
+		google.load('search', '1', {"callback" : onLoaded});
+	});
 	
-	jQuery.fn.google = function (query) {
+	function showQuery(el, query) {
 		var searchControl = new google.search.SearchControl();
 		searchControl.addSearcher(new google.search.ImageSearch());
-		searchControl.draw(this[0]);
+		searchControl.draw(el);
 		searchControl.execute(query);
+	}
+
+	jQuery.fn.google = function (query) {
+		var el = this[0];
+		deferred.done(function () {
+			showQuery(el, query);
+		});
 	};
 })(jQuery);
